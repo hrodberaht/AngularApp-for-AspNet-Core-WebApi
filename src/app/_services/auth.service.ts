9aefc3c130f0edb7e70bf7422fc26edaea2145ee
hrodberaht <robpszczolka@gmail.com>
2018-01-11 19:28:19 +0100

    add login functionality

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Headers } from '@angular/http/src/headers';
+import { Http, RequestOptions, Headers, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class AuthService {
     baseUrl = 'http://localhost:5000/api/auth';
+    userToken: any;
 
 constructor(private http: Http) { }
 
 login(model: any){
     const headers = new Headers({'Contetnt-type': 'application/json'});
-    return this.http.post(this.baseUrl + 'login', model, )
+    const options = new RequestOptions({headers: headers});
+    return this.http.post(this.baseUrl + '/login', model, options).map((response: Response)=>{
+        const user = response.json();
+        if(user){
+            localStorage.setItem( 'token', user.tokenString);
+            this.userToken = user.tokenString;
+        }
+    });
 }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 
 @NgModule({
@@ -21,7 +22,9 @@ import { NavComponent } from './nav/nav.component';
     HttpModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
