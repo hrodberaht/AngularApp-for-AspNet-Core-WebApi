a543a010b8a82411a58e12890309279255443e39
hrodberaht <robpszczolka@gmail.com>
2018-01-12 20:38:41 +0100

    working at register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   model: any = {};
+
+  @Input() valuesFromHome: any;
+
   constructor() { }
 
   ngOnInit() {
